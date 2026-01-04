d43a4d8619c8350d39abf9b3ff511ff08a72c762
Allen Tripp <13400103+atizzle07@users.noreply.github.com>
2026-01-04 01:19:21 -0600

    Add files accidentally ignored

--- Calculator/CalculatorApp/Program.cs
+++ Calculator/CalculatorApp/Program.cs
@@ -1,14 +1,15 @@
 ﻿// Program.cs
+using System.Text.RegularExpressions;
 using CalculatorLibrary;
 
 namespace CalculatorProgram
 {
-
     class Program
     {
         static void Main(string[] args)
         {
             bool endApp = false;
+            int calculatorUsed = 0;
             // Display title as the C# console calculator app.
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
@@ -55,28 +56,30 @@ namespace CalculatorProgram
                 string? op = Console.ReadLine();
 
                 // Validate input is not null, and matches the pattern
-                if (op == null || ! Regex.IsMatch(op, "[a|s|m|d]"))
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
                 {
-                   Console.WriteLine("Error: Unrecognized input.");
+                    Console.WriteLine("Error: Unrecognized input.");
                 }
                 else
-                { 
-                   try
-                   {
-                       result = calculator.DoOperation(cleanNum1, cleanNum2, op); 
-                       if (double.IsNaN(result))
-                       {
-                           Console.WriteLine("This operation will result in a mathematical error.\n");
-                       }
-                       else Console.WriteLine("Your result: {0:0.##}\n", result);
-                   }
-                   catch (Exception e)
-                   {
-                       Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
-                   }
+                {
+                    try
+                    {
+                        result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    }
+
+
                 }
                 Console.WriteLine("------------------------\n");
-
+                calculatorUsed += 1
                 // Wait for the user to respond before closing.
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 if (Console.ReadLine() == "n") endApp = true;
